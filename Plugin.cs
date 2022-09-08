97d5d3125f08ebddcada85a90430f71dd5adcfaa
radsi <78619513+radsi@users.noreply.github.com>
2022-09-08 08:47:02 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FakUPunch
+{
+    [BepInPlugin("Radsi", "FakUPunch", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static AssetBundle punchbundle;
+
+        private void Awake()
+        {
+            punchbundle = AssetBundle.LoadFromMemory(Resource1.punchanimation);
+            new Harmony("radsi.fakupunch").PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Punch), "Start")]
+        public static class patchAnimOnStart
+        {
+            public static void Postfix(Punch __instance)
+            {
+                __instance.anim.runtimeAnimatorController = punchbundle.LoadAsset<RuntimeAnimatorController>("Punch");
+            }
+        }
+
+        [HarmonyPatch(typeof(Punch), "OnEnable")]
+        public static class patchAnimOnEnable
+        {
+            public static void Postfix(Punch __instance)
+            {
+                __instance.anim.runtimeAnimatorController = punchbundle.LoadAsset<RuntimeAnimatorController>("Punch");
+            }
+        }
+
+        [HarmonyPatch(typeof(Punch), "EquipAnimation")]
+        public static class patchAnimOnEA
+        {
+            public static void Postfix(Punch __instance)
+            {
+                __instance.anim.runtimeAnimatorController = punchbundle.LoadAsset<RuntimeAnimatorController>("Punch");
+            }
+        }
+    }
+}
